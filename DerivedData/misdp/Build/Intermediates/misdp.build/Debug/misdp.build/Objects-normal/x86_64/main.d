e6d9fc8f8b7355e28dabc979325b9f5a9e123f8e
Qi Zhang <zqtczqtc@gmail.com>
2015-03-08 13:22:42 -0700

    First commit

---
+++ DerivedData/misdp/Build/Intermediates/misdp.build/Debug/misdp.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/Qi/Dropbox/research/my\ work/MISDP-solver/misdp/misdp/main.cpp
---
+++ misdp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  misdp
+//
+//  Created by Qi Zhang on 3/8/15.
+//  Copyright (c) 2015 Qi Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
